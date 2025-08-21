932ed35c1c9d62a29618438505980898a88ed523
soofiya <soofiyaayoob786@gmail.com>
2025-08-21 17:35:03 +0530

    ho

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -2,35 +2,47 @@
  * @type {import('next').Metadata}
  */
 export const metadata = {
-    metadataBase: new URL('https://ppmgeneraltrading.com'),
-    icons: {
-      icon: '/favicon.ico',
-      apple: '/logo.webp',
-     
-    },
+  metadataBase: new URL("https://www.ppmgeneraltrading.com"),
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/logo.webp",
+  },
+  favicon: "/favicon.ico",
+
   title: "PPM General Trading - Premium A4 Supplier UAE",
-  description: "PPM General Trading - UAE based A4 sheet suppliers offering bulk ordering at affordable price. Trusted by businesses across the region. Supplying high-quality A4 copy paper in bulk to businesses, schools, and distributors with guaranteed quality and timely delivery.",
+  description:
+    "PPM General Trading - UAE based A4 sheet suppliers offering bulk ordering at affordable price. Trusted by businesses across the region. Supplying high-quality A4 copy paper in bulk to businesses, schools, and distributors with guaranteed quality and timely delivery.",
   keywords: [
-    'PPM General Trading', 'A4 copy paper', 'bulk paper supplier','best A4 paper supplier', 'UAE',
-    'UAE paper supplier', 'wholesale A4 paper', 'premium A4 sheets',
-    'business paper supply', 'affordable A4 paper', 'uae paper suppliers', 'wholesale'
+    "PPM General Trading",
+    "A4 copy paper",
+    "bulk paper supplier",
+    "best A4 paper supplier",
+    "UAE",
+    "UAE paper supplier",
+    "wholesale A4 paper",
+    "premium A4 sheets",
+    "business paper supply",
+    "affordable A4 paper",
+    "uae paper suppliers",
+    "wholesale",
   ],
-  authors: [{ name: 'PPM General Trading LLC' }],
+  authors: [{ name: "PPM General Trading LLC" }],
   robots: { index: true, follow: true },
   openGraph: {
     title: "PPM General Trading - Premium A4 Supplier UAE",
-    description: "PPM General Trading - UAE based A4 sheet suppliers offering bulk ordering at affordable price. Trusted by businesses across the region. Supplying high-quality A4 copy paper in bulk to businesses, schools, and distributors with guaranteed quality and timely delivery.",
-    url: 'https://www.ppmgeneraltrading.com',
-    siteName: 'PPM General Trading LLC',
+    description:
+      "PPM General Trading - UAE based A4 sheet suppliers offering bulk ordering at affordable price. Trusted by businesses across the region. Supplying high-quality A4 copy paper in bulk to businesses, schools, and distributors with guaranteed quality and timely delivery.",
+    url: "https://www.ppmgeneraltrading.com",
+    siteName: "PPM General Trading LLC",
     images: [
       {
-        url: '/Logo.svg',
+        url: "/Logo.svg",
         width: 1200,
         height: 630,
-        alt: 'PPM General Trading LLC - Premium A4 Supplier UAE',
+        alt: "PPM General Trading LLC - Premium A4 Supplier UAE",
       },
     ],
-    type: 'website',
-    locale: 'en_US',
+    type: "website",
+    locale: "en_US",
   },
 };
